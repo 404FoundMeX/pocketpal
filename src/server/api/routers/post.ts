67adb416ac57010a85ced269fdb86d789f0474ce
David Tech <dedakup@users.noreply.github.com>
2025-08-28 07:08:46 +0200

    fix: fix remaining linting and type errors

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const postRouter = createTRPCRouter({
-  create: publicProcedure.input(z.object({ title: z.string() })).mutation(async ({ ctx, input }) => {
+  create: publicProcedure.input(z.object({ title: z.string() })).mutation(async ({ input }) => {
     return {
       id: "1",
       title: input.title,
